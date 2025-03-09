a05a9dd7a4fa5ea16380ddd066d02235d43ffa57
RaulToledoProcopio <rtolpro@gmail.com>
2025-03-09 13:18:48 +0100

    Dockerfile

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb42165aec522e3be182826c1e48fee41bef9008")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb2243af518a0f74a12059ba11f66b869803a24")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
